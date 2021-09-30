f0a18d2bab89f3e88d7cb6dbfd553294d8340316
Jancleidsson Soares <jancleidsson.silva@remote.talkdesk.com>
2021-09-30 15:26:20 -0300

    Project creation files

---
+++ src/test/kotlin/com/jss/springWebKotlin/SpringWebKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jss.springWebKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
